8f2802b453659b8f03cb5b89498b301fffffc7a3
edwardngo <hoangso8000@gmail.com>
2022-06-26 10:51:01 +0700

    sanxeoto, choxe

--- crawler/car_integration/car_integration/spiders/carmudi.py
+++ crawler/car_integration/car_integration/spiders/carmudi.py
@@ -1,4 +1,3 @@
-# chua on
 import datetime
 import json
 import re
@@ -12,6 +11,14 @@ from scrapy.http import HtmlResponse
 from scrapy.utils.project import get_project_settings
 
 LIST_PRODUCT_XPATH = '//*[@class="list-info"]/div[1]/a/@href'
+TABLE_ROW_DETAILS_XPATH = '//*[@id="properites"]/div[2]/div/table/tbody/tr'
+TABLE_DATA_KEY_XPATH = 'td/strong/text()'
+TABLE_DATA_VALUE_XPATH = 'td[2]/text()'
+DETAILS_XPATH_V2 = '//*[@id="controller_area"]/div[1]/div[1]/div[4]/div'
+NAME_XPATH_V2 = 'text()'
+VALUE_XPATH_V2 = 'span'
+CARNAME_V1 = '//*[@id="pages-title-name"]/text()'
+CARNAME_V2 = '//*[@id="controller_area"]/div[1]/div[1]/div[2]/h1/text()'
 
 
 class CarmudiSpider(scrapy.Spider):
@@ -30,38 +37,36 @@ class CarmudiSpider(scrapy.Spider):
                 callback=self.parse_product,
                 # cb_kwargs=dict(price=price),
             )
-        # next_page = "https://bonbanh.com/oto/page,{}".format(self.next_page_number)
-        # self.next_page_number += 1
-        # if self.next_page_number == 501:
-        #     return
-        # while True:
-        #     if self.next_page_number == 501:
-        #         break
-        #     html = requests.get(
-        #         "https://www.carmudi.vn/request.ajax.php?mode=getListCarNew&pg={}&cat_id=0&cat_parent_id=0&condition=1".format(
-        #             self.next_page_number
-        #         )
-        #     ).content.decode("utf8")
-        #     html = json.loads(html)["data"]
-        #     response_next_page = HtmlResponse(
-        #         url="next page", body=html, encoding="utf-8"
-        #     )
-        #     list_product = response_next_page.xpath(
-        #         '//*[@class="list-info"]/div[1]/a/@href'
-        #     ).getall()
-        #     self.next_page_number += 1
-        #     for product in list_product:
-        #         yield scrapy.Request(
-        #             url=product,
-        #             callback=self.parse_product,
-        #         )
+        next_page = "https://bonbanh.com/oto/page,{}".format(self.next_page_number)
+        self.next_page_number += 1
+        if self.next_page_number == 501:
+            return
+        while True:
+            if self.next_page_number == 501:
+                break
+            html = requests.get(
+                "https://www.carmudi.vn/request.ajax.php?mode=getListCarNew&pg={}&cat_id=0&cat_parent_id=0&condition=1".format(
+                    self.next_page_number
+                )
+            ).content.decode("utf8")
+            html = json.loads(html)["data"]
+            response_next_page = HtmlResponse(
+                url="next page", body=html, encoding="utf-8"
+            )
+            list_product = response_next_page.xpath(
+                '//*[@class="list-info"]/div[1]/a/@href'
+            ).getall()
+            self.next_page_number += 1
+            for product in list_product:
+                yield scrapy.Request(
+                    url=product,
+                    callback=self.parse_product,
+                )
 
     def parse_product(self, response):
         data = CarIntegrationItem(
             source=response.request.url,
-            # name=response.xpath('//*[@id="car_detail"]/div[3]/h1/text()')
-            # .get()
-            # .replace("\t", " "),
+            name='',
             base_url=self.base_url,
             time_update=datetime.datetime.utcnow(),
             image=[],
@@ -79,41 +84,53 @@ class CarmudiSpider(scrapy.Spider):
             mfg=None,
             status=""
         )
-        TABLE_ROW_DETAILS_XPATH = '//*[@id="properites"]/div[2]/div/table/tbody/tr'
-        TABLE_DATA_KEY_XPATH = 'td/strong'
-        TABLE_DATA_VALUE_XPATH = 'td[2]'
-        self.extract_details(response, data, TABLE_ROW_DETAILS_XPATH, TABLE_DATA_KEY_XPATH, TABLE_DATA_VALUE_XPATH)
-        DETAILS_XPATH_V2 = '//*[@id="controller_area"]/div[1]/div[1]/div[4]/div'
-        NAME_XPATH_V2 = 'text()'
-        VALUE_XPATH_V2 = 'span'
-        self.extract_details(response, data, DETAILS_XPATH_V2, NAME_XPATH_V2, VALUE_XPATH_V2)
+
+        details_v1 = response.xpath(TABLE_ROW_DETAILS_XPATH)
+        details_v2 = response.xpath(DETAILS_XPATH_V2)
+
+        if len(details_v1) != 0:
+            self.extract_details(data, details_v1, TABLE_DATA_KEY_XPATH, TABLE_DATA_VALUE_XPATH)
+        else:
+            self.extract_details(data, details_v2, NAME_XPATH_V2, VALUE_XPATH_V2)
+        
+        name_v1 = response.xpath(CARNAME_V1)
+        name_v2 = response.xpath(CARNAME_V2)
+        if len(name_v1) != 0 :
+            data['name'] = name_v1.get()
+        else:
+            data['name'] = name_v2.get()
 
 
-        # data["price"] = data["name"].split("-")[-1].strip()
+        price_v1 = response.xpath('//*[@id="car-price"]/text()')
+        price_v2 = response.xpath('//*[@id="controller_area"]/div[1]/div[1]/div[3]/div[2]/text()')
+        
+        data['price'] = price_v1.get().replace('\n','') if len(price_v1) else price_v2.get().replace('\n','')
+        
         regex = "\d{4}"
-        # data["mfg"] = re.findall(regex, data["name"])[-1]
+        try:
+            data["mfg"] = re.findall(regex, data["name"])[-1]
+        except:
+            data['mfg'] = ''
         data["image"] = response.xpath('//div[@id="medium_img"]/a/@href').getall()
-        # if data["engine"].split(" ")[0]:
-        #     data["fuel"] = data["engine"].split(" ")[0]
-        # data["engine"] = data["engine"].split(" ")[1]
-        # data["manufacturer"] = mapping_car_manufacturer(data["name"])
+        data["manufacturer"] = mapping_car_manufacturer(data["name"])
         
         yield data
 
-    def extract_details(self, response, data, row_details, name_xpath, value_xpath):
-        details = response.xpath(
-            row_details
-        )
+    def extract_details(self, data, details, name_xpath, value_xpath):
+        
         for detail in details:
-            if(len(detail.xpath(name_xpath)) > 1):
-                key = ''.join(detail.xpath(name_xpath).getall()).replace('\n','')
-                print(detail.xpath(name_xpath).getall())
+            if(len(detail.xpath(name_xpath).getall()) > 1):
+                key = ''.join(detail.xpath(name_xpath).getall()).strip().replace('\n','').split(':')[0]
             else:
                 key = detail.xpath(name_xpath).get().strip()
             remove_html_key = re.sub('<[^>]*>', '', key)
             field = mapping(remove_html_key)
             if field:
-                value = detail.xpath(value_xpath).get().replace("\t", " ")
-                data[field] = (
-                    re.sub('<[^>]*>', '', value)
-                )
+                value = detail.xpath(value_xpath).get()
+                if value == None:
+                    value = ''.join(detail.xpath(name_xpath).getall()).strip().replace('\n','').split(': ')[-1]
+                    data[field] = value
+                else:
+                    data[field] = (
+                        re.sub('<[^>]*>', '', value.replace("\t", " "))
+                    )
