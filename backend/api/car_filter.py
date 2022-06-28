847040e7ab531c8955d5a234791b26f5c81bdd7f
hieunm <hieunm.research@gmail.com>
2022-06-28 23:23:47 +0700

    update api backend call

--- backend/api/car_filter.py
+++ backend/api/car_filter.py
@@ -3,8 +3,4 @@ from backend import mongodb
 
 class CarFilter():
     def __init__(self):
-        self.car_db = mongodb['car']
-    
-    # def find(self, fields):
-    #     for field in fields:
-            
+        self.car_db = mongodb['car']      
--- backend/api/search.py
+++ backend/api/search.py
@@ -1,8 +1,10 @@
 import flask
-import pymongo
-from backend import mongodb
+import json
+
 from backend.api.car_filter import CarFilter
 from flask import request
+from bson import json_util
+
 
 car_filter = CarFilter()
 
@@ -20,6 +22,50 @@ def create_app():
         car_cursor = car_filter.car_db.find({'price': { "$gt": 3699000000 }})
         fields = car_cursor[0].keys()
         request_fields = {field: request.args.get(field) for field in fields}
+        request_fields['price_sort']  = request.args.get('price_sort')
+        request_fields['seat_sort']  = request.args.get('seat_sort')
+        request_fields['mfg_sort']  = request.args.get('mfg_sort')
         # request.args["deviceid"] if "deviceid" in request.args else None
-        print(request_fields)
+        query = {}
+        for key, value in request_fields.items():
+            if key == 'price':
+                if request_fields['price_sort'] == 'gt':
+                    query[key] = {'$gt': int(value)}
+                    continue
+                elif request_fields['price_sort'] == 'lt':
+                    query[key] = {'$lt': int(value)}
+                    continue
+                    
+            if key == 'seat':
+                if request_fields['seat_sort'] == 'gt':
+                    query[key] = {'$gt': int(value)}
+                elif request_fields['seat_sort'] == 'lt':
+                    query[key] = {'$lt': int(value)}
+                    
+            if key == 'mfg':
+                if request_fields['mfg_sort'] == 'gt':
+                    query[key] = {'$gt': int(value)}
+                elif request_fields['mfg_sort'] == 'lt':
+                    query[key] = {'$lt': int(value)}
+    
+            if key in ['price_sort', 'seat_sort', 'mfg_sort']:
+                continue
+            
+            if value is not None:
+                query[str(key)] = value
+        car_cursor = list(car_filter.car_db.find(query))
+        result = get_result_format(car_cursor)
+        return result
+    
+    def parse_json(data):
+        return json.loads(json_util.dumps(data))
+    
+    def get_result_format(rec_list):
+        result = dict()
+        d = []
+        for item_id in rec_list:
+            d.append(parse_json(item_id))
+        result["result"] = d
+        return result
+    
     return app
