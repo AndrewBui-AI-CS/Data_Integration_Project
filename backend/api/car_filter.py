37317b8a65009b0db53c3633ea71903cd550e41a
hieunm <hieunm.research@gmail.com>
2022-06-30 21:44:59 +0700

    fix api docs, code style

--- backend/api/car_filter.py
+++ backend/api/car_filter.py
@@ -3,4 +3,4 @@ from backend import mongodb
 
 class CarFilter():
     def __init__(self):
-        self.car_db = mongodb['car']      
+        self.car_db = mongodb['car']
--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,6 @@ from backend.api import search
 
 if __name__ == '__main__':
     search = search.create_app()
-    search.run(host='0.0.0.0', port=5000, debug=False)
+    search.run(host='127.0.0.1', port=5000, debug=False)
 else:
     gunicorn_app = search.create_app()
